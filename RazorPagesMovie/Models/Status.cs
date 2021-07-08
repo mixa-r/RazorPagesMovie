4b03b1ab14d93a6b687b5f8dd765892e7a906020
mixa-r <53909744+mixa-r@users.noreply.github.com>
2021-07-08 21:32:43 +0500

    Добавьте файлы проекта.

---
+++ RazorPagesMovie/Models/Status.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RazorPagesMovie.Models
+{
+    public enum TimeOfDay
+    {
+        [Display(Name = "Утро")]
+        Morning,
+        [Display(Name = "День")]
+        Afternoon,
+        [Display(Name = "Вечер")]
+        Evening,
+        [Display(Name = "Ночь")]
+        Night
+    }
+}
